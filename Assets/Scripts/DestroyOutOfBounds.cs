7fb61bb68eb27c21ab25fcebae69fe46182c1526
Vince van Doorn <323888>
2019-12-01 23:00:01 +0100

    2.4

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -11,11 +11,16 @@ public class DestroyOutOfBounds : MonoBehaviour
     }
 
     private float topBound = 30;
-    private float lowerBound = 30;
+    private float lowerBound = -10;
     // Update is called once per frame
     void Update()
     {
         if (transform.position.z > topBound) {
-            Destroy(gameObject); }
+            Destroy(gameObject); 
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }

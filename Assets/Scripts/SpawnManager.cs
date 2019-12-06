60061c462e0265c9ec99f7a4c5757a08f08c997e
Vince van Doorn <328388@student.mboutrecht.nl>
2019-12-06 11:07:13 +0100

    2.4 af collisions fixed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,12 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -19,12 +21,18 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            int animalIndex = Random.Range(0,animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            Instantiate(animalPrefabs[animalIndex], spawnPos ,animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
+            
         }
     }
-    void SpawnRandomAnimal() {}
+
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0,animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos ,animalPrefabs[animalIndex].transform.rotation);
+        
+    }
     
     
 }
